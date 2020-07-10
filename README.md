2af952f5c100e619408dec4fc47f5068554a8aff
Renato Aguiar <renatooaa@outlook.com>
2020-07-09 23:30:32 -0300

    update

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
     - Go to settings file;
     - Add AUTH_USER_MODEL = 'app.Class'
 4. `docker-compose run app sh -c "python manage.py makemigrations core"`
+5. `Learn about flake8`
+6. `docker-compose run app sh -c "python manage.py createsuperuser"`
+7. `docker-compose run --rm app sh -c "python manage.py startapp user"`
---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+CREATE_USER_URL = reverse('user:create')
+
+
+def create_user(**params):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Test the users API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+    
+    def test_create_valid_user_success(self):
+        """Test creating user"""
+        payload = {
+            'email': 'test@teste.com',
+            'password': '123password',
+            'name': 'Test'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
+    
+    def test_user_exists(self):
+        """Test creating user already exists fail"""
+        payload = {'email': 'test@test.com', 'password': '123password'}
+        create_user(**payload)
+
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+    
